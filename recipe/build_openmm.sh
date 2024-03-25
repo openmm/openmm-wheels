16fea414cc762dca0b5ed6c4ff897f1a18a5e577
Isuru Fernando <isuruf@gmail.com>
2024-03-25 11:36:59 -0500

    _GLIBCXX_USE_CXX11_ABI=0

--- recipe/build_openmm.sh
+++ recipe/build_openmm.sh
@@ -51,6 +51,7 @@ CMAKE_FLAGS+=" -DSWIG_EXECUTABLE=$(which swig)"
 
 if [[ "$target_platform" == linux-* ]]; then
     export LDFLAGS="$LDFLAGS -static-libstdc++ -Wl,--exclude-libs,ALL -Wl,-rpath,$ORIGIN/../OpenMM.libs/lib"
+    export CXXFLAGS=$CXXFLAGS -D_GLIBCXX_USE_CXX11_ABI=0"
 elif [[ "$target_platform" == osx-* ]]; then
     export LDFLAGS="$LDFLAGS -Wl,-rpath,@loader_path/../OpenMM.libs/lib -Wl,-rpath,@loader_path/."
 fi
